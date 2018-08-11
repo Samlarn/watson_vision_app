aded5eefd8c1ce39fe2b1850483e40ccf304ccfd
Nicklas Jonsson <jonsson.nicklas1990@gmail.com>
2018-08-11 17:13:20 +0200

    comm panel

--- src/main/java/view/CommunicationPanel.java
+++ src/main/java/view/CommunicationPanel.java
@@ -7,18 +7,51 @@ import java.awt.event.ActionListener;
 public class CommunicationPanel extends JPanel {
 
     private JTextField apiKeyField;
+    private JTextField classifierIdsField;
+
     private JButton setApiKeyBtn;
 
     public CommunicationPanel(String title) {
-        apiKeyField = new JTextField(27);
-        setApiKeyBtn = new JButton("Set");
+        Dimension size = new Dimension(455, 355);
+        this.setPreferredSize(size);
 
+        this.apiKeyField = new JTextField(25);
+        this.classifierIdsField = new JTextField(25);
+
+        this.setApiKeyBtn = new JButton("Set");
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(APIKeyBoxPanel());
+        this.add(classifierIdPanel());
+    }
+
+    /**
+     * Top panel box, the API-key input.
+     * @return the top Panel.
+     */
+    private JPanel APIKeyBoxPanel() {
+        JPanel panel = new JPanel();
         FlowLayout flowLayout = new FlowLayout();
-        this.setLayout(flowLayout);
-        this.add(new JLabel("API key: "), flowLayout);
-        this.add(apiKeyField, flowLayout);
-        this.add(setApiKeyBtn, flowLayout);
+        panel.setLayout(flowLayout);
+        panel.add(new JLabel("API key: "), flowLayout);
+        panel.add(apiKeyField, flowLayout);
+        panel.add(setApiKeyBtn, flowLayout);
+        return panel;
+    }
 
+    /**
+     * Classifier IDs panel.
+     * @return the classifier ids panel.
+     */
+    private JPanel classifierIdPanel() {
+        JPanel panel = new JPanel();
+        FlowLayout flowLayout = new FlowLayout();
+        panel.setLayout(flowLayout);
+        panel.add(new JLabel("Classifier ids: "), flowLayout);
+        this.classifierIdsField.setText("\"lantmannen_994293677\",\"bread_1474507279\"");
+        panel.add(classifierIdsField, flowLayout);
+        return panel;
     }
 
     /**
@@ -33,6 +66,10 @@ public class CommunicationPanel extends JPanel {
      */
     public void hideAPIKey() {
         apiKeyField.setText("*******************************");
+    }
+
+    public String getClassifierIds() {
+        return this.classifierIdsField.getText();
     }
 
     /**
